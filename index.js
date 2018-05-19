8f033e81cb0404ea5540d1890c73f4f121fd27a8
zhouzhi <zhouzhi073859@outlook.com>
2018-05-19 18:19:27 +0800

    1.0.2 1.开始搭建基础架构

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './js/App';
 
-AppRegistry.registerComponent('StartRead99APP', () => App);
+AppRegistry.registerComponent('StartRead99APP', () => {
+  return App;
+});
--- js/App.js
+++ js/App.js
@@ -13,14 +13,30 @@ import {
 } from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: 'Press Cmd+R to reload,\n Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n Shake or press menu button for dev menu'
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
+});
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -37,22 +53,3 @@ export default class App extends Component<Props> {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
